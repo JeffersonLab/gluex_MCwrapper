d1750c98987a5034aa36e539d8bc0c858adf7322
T-Britton <tbritton@jlab.org>
2018-10-17 07:57:13 -0400

    more edits to bring code inline

--- gluex_MC.py
+++ gluex_MC.py
@@ -931,7 +931,7 @@ def main(argv):
                                         os.system(str(indir)+" "+COMMAND)
                                 else:
                                         if PROJECT_ID != 0:
-                                                findmyjob="SELECT ID FROM Jobs WHERE Project_ID="+str(PROJECT_ID)+" && RunNumber="+str(runs[0])+" && FileNumber="+str(FILENO)+" && NumEvts="+str(NUMEVTS)
+                                                findmyjob="SELECT ID FROM Jobs WHERE Project_ID="+str(PROJECT_ID)+" && RunNumber="+str(runs[0])+" && FileNumber="+str(BASEFILENUM+FILENUM_this_run+-1)+" && NumEvts="+str(num_this_file)
                                                 dbcursor.execute(findmyjob)
                                                 MYJOB = dbcursor.fetchall()
                                                 print len(MYJOB) 
@@ -968,7 +968,7 @@ def main(argv):
                                 os.system(str(indir)+" "+COMMAND)
                         else:
                                 if PROJECT_ID != 0:
-                                        findmyjob="SELECT ID FROM Jobs WHERE Project_ID="+str(PROJECT_ID)+" && RunNumber="+str(RUNNUM)+" && FileNumber="+str(FILENO)+" && NumEvts="+str(NUMEVTS)
+                                        findmyjob="SELECT ID FROM Jobs WHERE Project_ID="+str(PROJECT_ID)+" && RunNumber="+str(RUNNUM)+" && FileNumber="+str(BASEFILENUM+FILENUM+-1)+" && NumEvts="+str(num)
                                         dbcursor.execute(findmyjob)
                                         MYJOB = dbcursor.fetchall() 
                                         print len(MYJOB) 
