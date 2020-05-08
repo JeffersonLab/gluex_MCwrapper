52ea32319e2a66991f0d03b9d283f6341f12bff6
T-Britton <tjbritto09@gmail.com>
2020-05-08 10:29:10 -0400

    version increment for DEV

--- gluex_MC.py
+++ gluex_MC.py
@@ -44,8 +44,8 @@ try:
 except:
         pass
 
-MCWRAPPER_VERSION="2.4.1"
-MCWRAPPER_DATE="05/07/20"
+MCWRAPPER_VERSION="2.4.2"
+MCWRAPPER_DATE="05/08/20"
 
 #====================================================
 #Takes in a few pertinant pieces of info.  Creates (if needed) a swif workflow and adds a job to it.
