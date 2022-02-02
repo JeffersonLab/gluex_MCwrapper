e8c885cd753f9f5ddb1905edc9569e20d3b279a3
Peter Pauli <Peter.Pauli@glasgow.ac.uk>
2022-02-02 06:10:17 -0500

    Add swif2 functionality for interactive running

--- gluex_MC.py
+++ gluex_MC.py
@@ -130,6 +130,71 @@ def swif_add_job(WORKFLOW, RUNNO, FILENO,SCRIPT,COMMAND, VERBOSE,PROJECT,TRACK,N
         elif int(PROJECT_ID) < 0:
                 recordAttempt(abs(int(PROJECT_ID)),RUNNO,FILENO,"SWIF",SWIF_ID_NUM,COMMAND['num_events'],NCORES,RAM)
 
+def swif2_add_job(WORKFLOW, RUNNO, FILENO,SCRIPT,COMMAND, VERBOSE,ACCOUNT,PARTITION,NCORES,DISK,RAM,TIMELIMIT,OS,DATA_OUTPUT_BASE_DIR, PROJECT_ID):
+        STUBNAME=""
+        if(COMMAND['custom_tag_string'] != "I_dont_have_one"):
+                STUBNAME=COMMAND['custom_tag_string']+"_"
+        # PREPARE NAMES
+        STUBNAME = STUBNAME+str(RUNNO) + "_" + str(FILENO)
+
+        JOBNAME = WORKFLOW + "_" + STUBNAME
+
+        # CREATE ADD-JOB COMMAND
+        # job
+        #try removing the name specification
+
+        mkdircom="mkdir -p "+DATA_OUTPUT_BASE_DIR+"/log/"
+        status = subprocess.call(mkdircom, shell=True)
+
+
+        add_command = "swif2 add-job -workflow " + WORKFLOW #+ " -name " + JOBNAME
+        # account/partition (used to be project/track in swif)
+        add_command += " -account " + ACCOUNT + " -partition " + PARTITION
+        # resources
+        add_command += " -create -cores " + NCORES + " -disk " + DISK + " -ram " + RAM + " -time " + TIMELIMIT + " -os " + OS
+        # stdout
+        add_command += " -stdout " + DATA_OUTPUT_BASE_DIR + "/log/" + str(RUNNO) + "_stdout." + STUBNAME + ".out"
+        # stderr
+        add_command += " -stderr " + DATA_OUTPUT_BASE_DIR + "/log/" + str(RUNNO) + "_stderr." + STUBNAME + ".err"
+        # tags
+        add_command += " -tag run_number " + str(RUNNO)
+        # tags
+        add_command += " -tag file_number " + str(FILENO)
+        # script with options command
+        # add_command += " -fail-save-dir "+DATA_OUTPUT_BASE_DIR
+
+        add_command += " "+SCRIPT  +" "+ getCommandString(COMMAND,"SWIF")
+
+        if(VERBOSE == True):
+                print( "job add command is \n" + str(add_command))
+
+        if(int(NCORES)==1 and int(RAM[:-2]) >= 10 and RAM[-2:]=="GB" ):
+                print( "SciComp has a limit on RAM requested per thread, as RAM is the limiting factor.")
+                print( "This will likely cause an AUGER-SUBMIT error.")
+                print( "Please either increase NCORES or decrease RAM requested and try again.")
+                exit(1)
+        # ADD JOB
+        if add_command.find(';')!=-1 or add_command.find('&')!=-1 :#THIS CHECK HELPS PROTECT AGAINST A POTENTIAL HACK VIA CONFIG FILES
+                print( "Nice try.....you cannot use ; or &")
+                exit(1)
+        #status = subprocess.call(add_command.split(" "))
+        SWIF_ID_NUM="-1"
+
+        if( int(PROJECT_ID) <=0 ):
+                print(add_command)
+                jobSubout=subprocess.check_output(add_command.split(" "))
+                print(jobSubout)
+                idnumline=jobSubout.split("\n")[0].strip().split("=")
+
+                if(len(idnumline) == 2 ):
+                        SWIF_ID_NUM=str(idnumline[1])
+
+        if int(PROJECT_ID) > 0:
+                recordJob(PROJECT_ID,RUNNO,FILENO,SWIF_ID_NUM,COMMAND['num_events'])
+                #recordFirstAttempt(PROJECT_ID,RUNNO,FILENO,"SWIF",SWIF_ID_NUM,COMMAND['num_events'],NCORES,RAM)
+        elif int(PROJECT_ID) < 0:
+                recordAttempt(abs(int(PROJECT_ID)),RUNNO,FILENO,"SWIF",SWIF_ID_NUM,COMMAND['num_events'],NCORES,RAM)
+
 #====================================================
 #Takes in a few pertinant pieces of info and submits a job to qsub.  This has not been fully integrated into the autonomous side
 #missing the batch_ID scraping. And  missing better host recording (Stubbed as "QSUB" for now)
@@ -881,7 +946,7 @@ def showhelp():
         helpstring+= " cleangeant=[0/1] where 0 means that the geant step will not be cleaned up after use (default is 1)\n"
         helpstring+= " cleanmcsmear=[0/1] where 0 means that the mcsmear step will not be cleaned up after use (default is 1)\n"
         helpstring+= " cleanrecon=[0/1]where 0 means that the reconstruction step will not be cleaned up after running (default is 0)\n"
-        helpstring+= " batch=[0/1/2] where 1 means that jobs will be submitted, 2 will do the same as 1 but also run the workflow in the case of swif (default is 0 [interactive])\n"
+        helpstring+= " batch=[0/1/2] where 1 means that jobs will be submitted, 2 will do the same as 1 but also run the workflow in the case of swif(2) (default is 0 [interactive])\n"
         helpstring+= " logdir=[path] will direct the .out and .err files to the specified path for qsub\n"
         return helpstring
 
@@ -968,6 +1033,8 @@ def main(argv):
         # PROJECT INFO
         PROJECT    = "gluex"          # http://scicomp.jlab.org/scicomp/#/projects
         TRACK      = "simulation"     # https://scicomp.jlab.org/docs/batch_job_tracks
+        ACCOUNT    = "halld"          # https://scicomp.jlab.org/scicomp/slurmJob/slurmAccount
+        PARTITION  = "ifarm"          # https://scicomp.jlab.org/scicomp/slurmJob/slurmInfo
         CONDOR_MAGIC = []
         # RESOURCES for swif jobs
         NCORES     = "8"               # Number of CPU cores
@@ -1040,11 +1107,15 @@ def main(argv):
                                 VERBOSE=True
                 elif str(parts[0]).upper()=="PROJECT" :
                         PROJECT=rm_comments[0].strip()
+                elif str(parts[0]).upper()=="ACCOUNT" :
+                        ACCOUNT=rm_comments[0].strip()
                 elif str(parts[0]).upper()=="CONDOR_MAGIC" :
                         #print("detected magic")
                         CONDOR_MAGIC.append(rm_comments[0].strip())
                 elif str(parts[0]).upper()=="TRACK" :
                         TRACK=rm_comments[0].strip()
+                elif str(parts[0]).upper()=="PARTITION" :
+                        PARTITION=rm_comments[0].strip()
                 elif str(parts[0]).upper()=="NCORES" :
                         NCORES=rm_comments[0].strip()
                 elif str(parts[0]).upper()=="DISK" :
@@ -1347,6 +1418,9 @@ def main(argv):
         if (BATCHSYS.upper() == "SWIF") and int(BATCHRUN) != 0 and ccdbSQLITEPATH=="no_sqlite":
                 ccdbSQLITEPATH="jlab_batch_default"
                 LOCATION="JLAB"
+        if (BATCHSYS.upper() == "SWIF2") and int(BATCHRUN) != 0 and ccdbSQLITEPATH=="no_sqlite":
+                ccdbSQLITEPATH="jlab_batch_default"
+                LOCATION="JLAB"
         #if (BATCHSYS.upper() == "OSG" or BATCHSYS.upper() == "SWIF") and int(BATCHRUN) != 0 and rcdbSQLITEPATH=="no_sqlite":
         if (BATCHSYS.upper() == "OSG" ) and int(BATCHRUN) != 0 and rcdbSQLITEPATH=="no_sqlite":
                 rcdbSQLITEPATH="batch_default"
@@ -1454,6 +1528,11 @@ def main(argv):
                                         #status = subprocess.call("swif create "+WORKFLOW,shell=True)
                                         swif_add_job(WORKFLOW, RUNNUM, BASEFILENUM,str(SCRIPT_TO_RUN),COMMAND_dict,VERBOSE,PROJECT,TRACK,NCORES,DISK,RAM,TIMELIMIT,OS,DATA_OUTPUT_BASE_DIR, PROJECT_ID)
                                         swifrun = "swif run "+WORKFLOW
+                                        subprocess.call(swifrun.split(" "))
+                                elif BATCHSYS.upper()=="SWIF2":
+                                        #status = subprocess.call("swif2 create "+WORKFLOW,shell=True)
+                                        swif2_add_job(WORKFLOW, RUNNUM, BASEFILENUM,str(SCRIPT_TO_RUN),COMMAND_dict,VERBOSE,ACCOUNT,PARTITION,NCORES,DISK,RAM,TIMELIMIT,OS,DATA_OUTPUT_BASE_DIR, PROJECT_ID)
+                                        swifrun = "swif2 run "+WORKFLOW
                                         subprocess.call(swifrun.split(" "))
                                 elif BATCHSYS.upper()=="QSUB":
                                         qsub_add_job(VERBOSE, WORKFLOW, RUNNUM, BASEFILENUM, SCRIPT_TO_RUN, COMMAND_dict, NCORES, DATA_OUTPUT_BASE_DIR, TIMELIMIT, RUNNING_DIR, RAM, QUEUENAME, LOG_DIR, PROJECT_ID )
@@ -1580,6 +1659,9 @@ def main(argv):
                                                         if BATCHSYS.upper()=="SWIF":
                                                                 #status = subprocess.call("swif create "+WORKFLOW,shell=True)
                                                                 swif_add_job(WORKFLOW, runs[0], BASEFILENUM+FILENUM_this_run+-1,str(SCRIPT_TO_RUN),COMMAND_dict,VERBOSE,PROJECT,TRACK,NCORES,DISK,RAM,TIMELIMIT,OS,DATA_OUTPUT_BASE_DIR, PROJECT_ID)
+                                                        elif BATCHSYS.upper()=="SWIF2":
+                                                                #status = subprocess.call("swif2 create "+WORKFLOW,shell=True)
+                                                                swif2_add_job(WORKFLOW, runs[0], BASEFILENUM+FILENUM_this_run+-1,str(SCRIPT_TO_RUN),COMMAND_dict,VERBOSE,ACCOUNT,PARTITION,NCORES,DISK,RAM,TIMELIMIT,OS,DATA_OUTPUT_BASE_DIR, PROJECT_ID)
                                                         elif BATCHSYS.upper()=="QSUB":
                                                                 qsub_add_job(VERBOSE, WORKFLOW, runs[0], BASEFILENUM+FILENUM_this_run+-1, SCRIPT_TO_RUN, COMMAND_dict, NCORES, DATA_OUTPUT_BASE_DIR, TIMELIMIT, RUNNING_DIR, RAM, QUEUENAME, LOG_DIR, PROJECT_ID )
                                                         elif BATCHSYS.upper()=="CONDOR":
@@ -1630,6 +1712,9 @@ def main(argv):
                                                 if BATCHSYS.upper()=="SWIF":
                                                         #status = subprocess.call("swif create "+WORKFLOW,shell=True)
                                                         swif_add_job(WORKFLOW, RUNNUM, BASEFILENUM+FILENUM+-1,str(SCRIPT_TO_RUN),COMMAND_dict,VERBOSE,PROJECT,TRACK,NCORES,DISK,RAM,TIMELIMIT,OS,DATA_OUTPUT_BASE_DIR, PROJECT_ID)
+                                                elif BATCHSYS.upper()=="SWIF2":
+                                                        #status = subprocess.call("swif2 create "+WORKFLOW,shell=True)
+                                                        swif2_add_job(WORKFLOW, RUNNUM, BASEFILENUM+FILENUM+-1,str(SCRIPT_TO_RUN),COMMAND_dict,VERBOSE,ACCOUNT,PARTITION,NCORES,DISK,RAM,TIMELIMIT,OS,DATA_OUTPUT_BASE_DIR, PROJECT_ID)
                                                 elif BATCHSYS.upper()=="QSUB":
                                                         qsub_add_job(VERBOSE, WORKFLOW, RUNNUM, BASEFILENUM+FILENUM+-1, SCRIPT_TO_RUN, COMMAND_dict, NCORES, DATA_OUTPUT_BASE_DIR, TIMELIMIT, RUNNING_DIR, RAM, QUEUENAME, LOG_DIR, PROJECT_ID )
                                                 elif BATCHSYS.upper()=="CONDOR":
@@ -1647,6 +1732,12 @@ def main(argv):
                         print( "All Jobs created.  Please call \"swif run "+WORKFLOW+"\" to run")
                 elif BATCHRUN == 2 and BATCHSYS.upper()=="SWIF":
                         swifrun = "swif run "+WORKFLOW
+                        subprocess.call(swifrun.split(" "))
+
+                if BATCHRUN == 1 and BATCHSYS.upper() == "SWIF2":
+                        print( "All Jobs created.  Please call \"swif2 run "+WORKFLOW+"\" to run")
+                elif BATCHRUN == 2 and BATCHSYS.upper()=="SWIF2":
+                        swifrun = "swif2 run "+WORKFLOW
                         subprocess.call(swifrun.split(" "))
 
         try:
